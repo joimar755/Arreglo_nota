f08ae68e263c4425be86a8b0dc604912c2ce2790
joimar755 <joimarjose19@gmail.com>
2022-04-06 23:50:35 -0500

    OPP

--- src/main/java/Matriz_Ejercicios/Ejercicio1.java
+++ src/main/java/Matriz_Ejercicios/Ejercicio1.java
@@ -8,13 +8,13 @@ import javax.swing.JOptionPane;
 
 public class Ejercicio1 {
 
-    int n = 3;
+    int n = 5;
     int a[][] = new int[n][n];
     int mitad = a[0].length / 2;
     int mitad_f = a.length / 2;
 
     double suma = 0;
-    double sumai = 0, sumaC = 0, sumaF = 0;
+    double sumai = 0, sumaC = 0, sumaF = 0, sumaD = 0, sumaS = 0;
 
     public void Cargar_matriz() {
 
@@ -72,20 +72,34 @@ public class Ejercicio1 {
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[0].length; j++) {
                 if (i == mitad_f) {
+                    System.out.println("---------diagonal_fila----------");
                     System.out.println(a[i][j]);
                     sumaC += a[i][j];
                 }
                 if (j == mitad) {
+                    System.out.println("---------mitad_columna----------");
                     System.out.println(a[i][j]);
                     sumaF += a[i][j];
 
+                }
+                if (i == j) {
+                    System.out.println("---------diagonal_principal----------");
+
+                    System.out.println(a[i][j]);
+                    sumaD += a[i][j];
+                }
+                if ((i + j) == a.length - 1) {
+                    System.out.println("---------diagonal_secundaria----------");
+                    System.out.println(a[i][j]);
+                    sumaS += a[i][j];
                 }
 
             }
 
         }
 
-        System.out.println("suma fila: " + sumaC + " suma columna: " + sumaF);
+        System.out.println("suma_fila: " + sumaC + " suma_columna: " + sumaF + " suma_diagonal: " + sumaD
+                + " suma_diagonal_secundaria: " + sumaS);
 
     }
 
